583de8dadd7b2e4933d70b1e37d0ffe9925355f1
wrodgers95 <will.rodgers1995@gmail.com>
2016-09-15 09:35:51 -0400

    commit 1

---
+++ src/main/java/com/theironyard/User.java
@@ -0,0 +1,21 @@
+package com.theironyard;
+
+public class User {
+
+    String userName;
+
+    public User(String userName) {
+        this.userName = userName;
+    }
+
+    public User() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
